9bdb19a5b72b66f19eaa85102c19fc5e0e96b3af
Mahendra <cmrajan@gmail.com>
2020-07-29 23:44:47 -0700

    try function

--- main.go
+++ main.go
@@ -1,14 +1,32 @@
 package main
 
 import (
+	"bytes"
+	"io/ioutil"
+
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
 func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+
+	files, err := ioutil.ReadDir(".")
+	if err != nil {
+		return events.APIGatewayProxyResponse{
+			StatusCode: 500,
+			Body:       err.Error(),
+		}, err
+	}
+
+	buf := bytes.NewBuffer(nil)
+
+	for _, file := range files {
+		buf.WriteString(file.Name() + "\n")
+	}
+
 	return events.APIGatewayProxyResponse{
 		StatusCode: 200,
-		Body:       "Hello AWS Lambda and Netlify",
+		Body:       "Hello AWS Lambda and search\n" + buf.String(),
 	}, nil
 }
 
