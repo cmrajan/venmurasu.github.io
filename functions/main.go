b399ba8b649fca06ccade8d805f3325b411808fd
Mahendra <cmrajan@gmail.com>
2020-07-29 22:57:55 -0700

    deploy

--- functions/main.go
+++ functions/main.go
@@ -1,32 +1,29 @@
 package main
 
 import (
-	"bytes"
-	"io/ioutil"
-
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
 func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 
-	files, err := ioutil.ReadDir(".")
-	if err != nil {
-		return events.APIGatewayProxyResponse{
-			StatusCode: 500,
-			Body:       err.Error(),
-		}, err
-	}
+	// files, err := ioutil.ReadDir(".")
+	// if err != nil {
+	// 	return events.APIGatewayProxyResponse{
+	// 		StatusCode: 500,
+	// 		Body:       err.Error(),
+	// 	}, err
+	// }
 
-	buf := bytes.NewBuffer(nil)
+	// buf := bytes.NewBuffer(nil)
 
-	for _, file := range files {
-		buf.WriteString(file.Name() + "\n")
-	}
+	// for _, file := range files {
+	// 	buf.WriteString(file.Name() + "\n")
+	// }
 
 	return events.APIGatewayProxyResponse{
 		StatusCode: 200,
-		Body:       "Hello AWS Lambda and Netlify Marty\n" + buf.String(),
+		Body:       "Hello AWS Lambda and Venmurasu\n",
 	}, nil
 }
 
